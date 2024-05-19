adb7bbe71bd2a20dae554294df42e5850bc2d814
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-19 11:02:00 +0100

    init midllware

---
+++ Middlewares/User_Middleware.js
@@ -0,0 +1,88 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { Refresh_tokens } = require("../Models/RefreshTokens");
+
+const verifyUser = async (req, res, next) => {
+    const accessToken = req.cookies.accessToken;
+    const refreshToken = req.cookies.refreshToken;
+
+    // if (!accessToken) {
+    //     return res.status(401).json({ message: "Access token required" });
+    // }
+
+    try {
+        const decoded = jwt.verify(
+            accessToken,
+            process.env.ACCESS_TOKEN_SECRET
+        );
+        if (!decoded)
+            return res
+                .status(401)
+                .json({ message: "unauthorized : Invalid access token" });
+        else if (decoded.userId != req.params.userId)
+            return res
+                .status(401)
+                .json({ message: "unauthorized : Invalid access token" });
+        req.decoded = decoded;
+        return next();
+    } catch (err) {
+        if (err.name === "TokenExpiredError") {
+            if (!refreshToken) {
+                return res
+                    .status(401)
+                    .json({ message: "unauthorized : Refresh token required" });
+            }
+
+            try {
+                const foundInDB = await Refresh_tokens.findOne({
+                    where: {
+                        token: refreshToken,
+                    },
+                });
+
+                if (!foundInDB) {
+                    return res.status(403).json({
+                        message: "unauthorized : Invalid refresh token",
+                    });
+                }
+
+                jwt.verify(
+                    refreshToken,
+                    process.env.REFRESH_TOKEN_SECRET,
+                    async (err, decoded) => {
+                        if (err || foundInDB.userId !== decoded.userId) {
+                            return res.status(403).json({
+                                message: "unauthorized : Invalid refresh token",
+                            });
+                        }
+
+                        const newAccessToken = jwt.sign(
+                            { userId: decoded.userId },
+                            process.env.ACCESS_TOKEN_SECRET,
+                            { expiresIn: "1h" }
+                        );
+
+                        res.cookie("accessToken", newAccessToken, {
+                            httpOnly: true,
+                            sameSite: "None",
+                            secure: true,
+                            maxAge: 60 * 60 * 1000,
+                        });
+
+                        req.decoded = decoded;
+                        return next();
+                    }
+                );
+            } catch (refreshErr) {
+                return res
+                    .status(403)
+                    .json({ message: "Invalid refresh token" });
+            }
+        } else {
+            return res.status(401).json({ message: "Invalid access token" });
+        }
+    }
+};
+
+module.exports = verifyUser;
