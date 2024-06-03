7ec02563cf3fd94766dc832fad944df1e8eae9f1
imadbenmadi <imadbenmadi891@gmail.com>
2024-06-03 14:05:00 +0100

    fix get project

--- Controllers/Client/Project/GetProject.js
+++ Controllers/Client/Project/GetProject.js
@@ -1,22 +1,23 @@
 // const { Clients } = require("../../Models/Client");
 const { Projects } = require("../../../Models/Project");
-const GetProjcts = async (req, res) => {
-    const projectId = req.body.projectId;
+const GetProject = async (req, res) => {
+    const projectId = req.params.projectId;
     const userId = req.decoded.userId;
     if (!userId)
         return res.status(401).json({ error: "Unauthorized , missing userId" });
     try {
-        const projects = await Projects.findOne({
+        const project = await Projects.findOne({
             where: {
                 id: projectId,
                 ClientId: userId,
             },
         });
-        return res.status(200).json({ Projects: projects });
+        // console.log("got project : ", project);
+        return res.status(200).json({ Project: project });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "Internal server error." });
     }
 };
 
-module.exports = { GetProjcts };
+module.exports = { GetProject };
