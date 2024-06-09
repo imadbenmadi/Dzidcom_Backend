5eb811ba7271bccd2c1b77dabaa92236eae14ec6
imadbenmadi <imadbenmadi891@gmail.com>
2024-06-09 19:24:40 +0100

    chore: Update Home route and fetch counts for freelancers, clients, and projects

--- Routes/Admin/Home.js
+++ Routes/Admin/Home.js
@@ -9,29 +9,40 @@ const { Projects } = require("../../Models/Project");
 const { Applications } = require("../../Models/Applications");
 router.get("/", Admin_midllware, async (req, res) => {
     try {
-        const freelancers = await Freelancers.count({
+        const freelancers_nbr = await Freelancers.count({
             where: {},
         });
-        const clients = await Clients.count({
+        const clients_nbr = await Clients.count({
             where: {},
         });
-        const projects = await Projects.count({
+        const projects_nbr = await Projects.count({
+            where: { status: "Accepted" },
+        });
+        // const payments = await Projects.count({
+        //     where: { status: "Payed" },
+        // });
+        // const applications = await Applications.count({
+        //     where: {},
+        // });
+        const freelancers = await Freelancers.findAll({
             where: {},
         });
-        const payments = await Projects.count({
-            where: { status: "Payed" },
-        });
-        const applications = await Applications.count({
+        const clients = await Clients.findAll({
             where: {},
         });
-        if (!freelancers) freelancers = 0;
-        if (!clients) clients = 0;
+        const projects = await Projects.findAll({
+            where: { status: "Accepted" },
+        });
+        if (!freelancers_nbr) freelancers_nbr = 0;
+        if (!clients_nbr) clients_nbr = 0;
+        if (!projects_nbr) projects_nbr = 0;
         res.status(200).json({
-            Freelancers: freelancers,
-            Clients: clients,
-            Projects: projects,
-            Payments: payments,
-            Applications: applications,
+            freelancers_nbr: freelancers_nbr,
+            clients_nbr: clients_nbr,
+            projects_nbr: projects_nbr,
+            freelancers: freelancers,
+            clients: clients,
+            projects: projects,
         });
     } catch (err) {
         console.error("Error fetching Project Applications:", err);
